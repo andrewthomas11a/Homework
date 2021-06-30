c0e6ef973ecf10ed645a7df719d4428b43225872
Andrew Titov <andrewthomas11a@gmail.com>
2021-06-30 18:44:41 +0300

    hm7 - 6

--- src/home_work_7/MainAppMultiThread.java
+++ src/home_work_7/MainAppMultiThread.java
@@ -2,13 +2,16 @@ package home_work_7;
 
 import home_work_7.api.ISearchEngine;
 import home_work_7.searchUtils.RegExSearch;
-import utils_temp.InputCheck;
+
 import java.io.*;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
-public class MainApp {
+public class MainAppMultiThread {
     public static void main(String[] args) throws IOException {
 
         String userPath = "Homework\\books";
@@ -21,20 +24,8 @@ public class MainApp {
             for (int i = 0; i < list.length; i++) {
                 System.out.println(i + 1 + ". " + list[i]);
             }
-            System.out.println("Выберите нужную книгу. Для выбора напишите номер и нажмите Enter.");
-            InputCheck inputBookNumber = new InputCheck("Нужно ввести номер книги, попробуйте еще раз.");
-            int numberOfBook = inputBookNumber.checkIntInput();
-            while (numberOfBook < 0 || numberOfBook > list.length) {
-                System.out.println("Книги с таким номером нет в списке! Попрообуйте еще раз.");
-                numberOfBook = inputBookNumber.checkIntInput();
-            }
 
-            System.out.println("Вы выбрали книгу \"" + list[numberOfBook - 1] + "\".");
-
-            // Может читать только UTF-8, судя по документации
-            // при попытке чтения файла другой кодировки выдает MalformedInputException
-            String text = Files.readString(Path.of("Homework\\books\\" + list[numberOfBook - 1]));
-
+            System.out.println("\n" + "Поиск будет осуществляться во всех книгах.");
             System.out.println("Очистить историю поиска и начать заново? Напишите \"yes\" для очистки или \"no\" для продолжения и нажмите Enter:");
             String userWantToClear = userInput.readLine();
             while (!userWantToClear.equals("yes") && !userWantToClear.equals("no")) {
@@ -45,27 +36,44 @@ public class MainApp {
             if (userWantToClear.equals("yes")) {
                 clearOrNot = true;
             }
+
             // вложенный try-catch.
             // не знаю как по-другому в конструктор передать полученное в ходе выполнения программы значение
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Homework\\result.txt", !clearOrNot));) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Homework\\result2.txt", !clearOrNot));) {
                 System.out.println("Вводите искомое слово. Напишите \"stop\" чтобы закончить");
-                ISearchEngine findWord = new RegExSearch();
-                long result;
+                ExecutorService exe;
                 while (true) {
+                    exe = Executors.newFixedThreadPool(list.length);
                     // получаем от пользователя слово для поиска
                     String word = userInput.readLine();
                     // прерываем цикл если введено stop-слово
                     if (word.equals("stop")) {
                         break;
                     }
-                    // если введено что-то еще - ищем слово в тексте
-                    result = findWord.search(text, word);
-                    // записываем в текстовый файл
-                    writer.write("\"" + list[numberOfBook - 1] + "\" - " + word + " - " + result+"\n");
+                    List<Future<Long>> tasksList = new ArrayList<>();
+                    for (int i = 0; i < list.length; i++) {
+                        Future<Long> submit = exe.submit(new SearchInFile(list[i], word, writer));
+                        tasksList.add(submit);
+                    }
+
+                    long result = 0;
+                    try {
+                        for (Future<Long> futureTast : tasksList) {
+                            long tempResult = futureTast.get();
+                            result += tempResult;
+                        }
+                        writer.write("* * * * * Слово \""+word+"\" найдено во всех файлах "+result+" раз(а). * * * * *"
+                                + "\n\n");
+                    } catch (InterruptedException | ExecutionException e) {
+                        exe.shutdown();
+                        System.out.println("Во время обработки запроса произошла ошибка, попробуйте еще раз.");
+                    }
+                    exe.shutdown();
                 }
                 writer.flush();
+                exe.shutdown();
             }
-            System.out.println("Результаты поиска были записаны в файл result.txt в директории Homework");
+            System.out.println("Результаты поиска были записаны в файл result2.txt в директории Homework");
         } catch (NullPointerException e) {
             System.out.println("Что-то не то со ссылкой, в указанной директории ничего нет.");
         } catch (MalformedInputException e) {
@@ -73,5 +81,27 @@ public class MainApp {
         } catch (IOException e) {
             System.out.println("Во время работы с файлом что-то пошло не так. Запустите программу заново");
         }
+    }
+}
+
+class SearchInFile implements Callable<Long> {
+    String fileName;
+    String word;
+    BufferedWriter writer;
+
+    SearchInFile (String fileName, String word, BufferedWriter writer) {
+        this.fileName = fileName;
+        this.word = word;
+        this.writer = writer;
+    }
+
+    @Override
+    public Long call() throws IOException {
+        long result;
+        String bookText = Files.readString(Path.of("Homework\\books\\" + fileName));
+        ISearchEngine findWord = new RegExSearch();
+        result = findWord.search(bookText, word);
+        writer.write("\"" + fileName + "\" - " + word + " - " + result+"\n");
+        return result;
     }
 }
